f92b8ca0786caeeb34930e7445eb1d0f76dffbd7
Yoko Parks <hellothereyoko@gmail.com>
2026-04-07 12:53:06 -0700

    Made a Main Menu
    Renamed Sample Scene to Level_01 to hook into my code when the player hits new game. DONT CHANGE IT WITHOUT UPDATING THE MENU CONTROLLER SCRIPT

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This script serves to control the main menu system in the game. 
+ * I heavily modified a script given to me by Claude. It relied on older systems that weren't maintained so I adapted it to the
+ * new UI control methods and added additional UI debugs to help me while hooking it up!
+ * 
+ * Made by: Yoko Parks 
+*/
+public class MainMenuController : MonoBehaviour
+{
+    //Mks new game & save. Then, it proceeds to load lvl 1
+    public void NewGame()
+        => SceneManager.LoadScene("Level_01"); 
+
+    //Triggers Scene of the aforementioned name to load 
+    public void LoadGame()
+        => SceneManager.LoadScene("Level_Load");
+
+    //Loads Options Menu
+    public void OpenOptions()
+    {
+      Debug.Log("Options Button Activated!");
+      optionsPanel.SetActive(true);
+    }
+
+    //This one is pretty self-explainatory 
+    public void QuitGame()
+
+    {
+      Debug.Log("Quit Button Activated!");
+      Application.Quit();
+    }
+
+    [SerializeField] GameObject optionsPanel;
+}
