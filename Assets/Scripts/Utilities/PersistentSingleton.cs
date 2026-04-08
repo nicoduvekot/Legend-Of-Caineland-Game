3d1cd71e459d86d284cd8205e47d31d19100c822
Nico Duvekot <nduvekot7@gmail.com>
2026-04-08 15:02:59 -0700

    Add : PersistentSingleton
    this make a singleton type that can persist through scenes - great for the ability to always have the single instance for manipulation of GameData

---
+++ Assets/Scripts/Utilities/PersistentSingleton.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public class PersistentSingleton <T> : MonoBehaviour where T : Component
+    {
+        public bool autoUnparentOnAwake = true;
+
+        protected static T _instance;
+        
+        public static bool HasInstance => _instance != null;
+
+        public static T TryGetInstance() => HasInstance ? _instance : null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject go = new(typeof(T).Name);
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying) return;
+
+            if (autoUnparentOnAwake)
+            {
+                transform.SetParent(null);
+            }
+
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (_instance != this)
+                    Destroy(gameObject);
+            }
+        }
+    }
+}
