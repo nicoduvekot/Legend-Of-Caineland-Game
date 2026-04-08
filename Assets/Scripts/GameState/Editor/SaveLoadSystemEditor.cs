d152b9b02410744d16fbd8a651095ecc4a49288d
Nico Duvekot <nduvekot7@gmail.com>
2026-04-08 15:13:50 -0700

    Create : SaveLoadSystemEditor
    Editor display setup for SaveLoad usage via editor

---
+++ Assets/Scripts/GameState/Editor/SaveLoadSystemEditor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using GameState.Core;
+using GameState.SaveLoad;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameState.Editor
+{
+    [CustomEditor(typeof(SaveLoadSystem))]
+    public class SaveLoadSystemEditor : UnityEditor.Editor
+    {
+        private int _selectedIndex;
+        private string [] _saveNames = Array.Empty<string>();
+        
+        public override void OnInspectorGUI()
+        {
+            SaveLoadSystem saveLoadSystem = (SaveLoadSystem)target;
+
+            if (saveLoadSystem.GetDataService() != null)
+            {
+                _saveNames = saveLoadSystem
+                    .GetDataService()
+                    .ListSaves()
+                    .ToArray();
+            }
+            
+            DrawDefaultInspector();
+            
+            EditorGUILayout.Space();
+            
+            string activeSaveName = Application.isPlaying
+                ? GameStateManager.Instance?.Data?.SaveName ?? "None"
+                : "(Play Mode Only)";
+            
+            EditorGUILayout.LabelField("Active Save:", activeSaveName);
+            
+            EditorGUILayout.Space();
+
+            if (Application.isPlaying)
+            {
+                if (GUILayout.Button("New Game"))
+                {
+                    saveLoadSystem.NewGame();
+                }
+
+                if (GUILayout.Button("Save Game"))
+                {
+                    saveLoadSystem.SaveGame();
+                }
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("Start New Game and Save Game require Play Mode.", MessageType.Info);
+            }
+            
+            EditorGUILayout.Space();
+            
+            EditorGUILayout.LabelField("Saves List:");
+            if (_saveNames.Length > 0)
+            {
+                _selectedIndex = EditorGUILayout.Popup("Select Save", _selectedIndex, _saveNames);
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("No saves found.", MessageType.Info);
+            }
+            
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Load Game") && _saveNames.Length > 0)
+            {
+                if (Application.isPlaying)
+                {
+                    saveLoadSystem.LoadGame(_saveNames[_selectedIndex]);
+                }
+                else
+                {
+                    Debug.LogWarning("Load Game requires Play Mode.");
+                }
+            }
+
+            if (GUILayout.Button("Delete Game") && _saveNames.Length > 0)
+            {
+                saveLoadSystem.DeleteGame(_saveNames[_selectedIndex]);
+            }
+        }
+    }
+}
