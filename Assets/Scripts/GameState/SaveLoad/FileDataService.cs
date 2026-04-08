44352dbcaafad1d19257903ee2db819734f9cef3
Nico Duvekot <nduvekot7@gmail.com>
2026-04-08 15:12:24 -0700

    Create : FileDataService
    Data Service for json to file using the player's local machine for save storage and retrieval

---
+++ Assets/Scripts/GameState/SaveLoad/FileDataService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace GameState.SaveLoad
+{
+    public class FileDataService : IDataService
+    {
+        private readonly ISerializer _serializer;
+        private readonly string _dataPath;
+        private readonly string _fileExtension;
+        
+        public FileDataService(ISerializer serializer)
+        {
+            _serializer = serializer;
+            _dataPath = Application.persistentDataPath;
+            _fileExtension = ".json";
+        }
+
+        private string GetPathToFile(string filename)
+        {
+            return Path.Combine(_dataPath, string.Concat(filename, _fileExtension));
+        }
+
+        public void Save(GameDataDTO data, bool overwrite = true)
+        {
+            string fileLocation = GetPathToFile(data.saveName);
+
+            if (!overwrite && File.Exists(fileLocation))
+                throw new IOException($"The File '{data.saveName}.{_fileExtension}' already exists and cannot be overwritten.");
+            
+            File.WriteAllText(fileLocation, _serializer.Serialize(data));
+        }
+
+        public GameDataDTO Load(string name)
+        {
+            string fileLocation = GetPathToFile(name);
+            
+            if (!File.Exists(fileLocation))
+                throw new ArgumentException($"No GameData with the name '{name}' was found.");
+            
+            return _serializer.Deserialize<GameDataDTO>(File.ReadAllText(fileLocation));
+        }
+
+        public void Delete(string name)
+        {
+            string fileLocation = GetPathToFile(name);
+            
+            if (File.Exists(fileLocation))
+                File.Delete(fileLocation);
+        }
+
+        public void DeleteAll()
+        {
+            // CAREFUL!! THIS DELETES EVERYTHING AT THIS LOCATION WITHOUT CHECKS!!
+            foreach (string file in Directory.GetFiles(_dataPath))
+                Delete(file);
+        }
+
+        public IEnumerable<string> ListSaves()
+        {
+            return from path in Directory.EnumerateFiles(_dataPath) where Path.GetExtension(path) == _fileExtension select Path.GetFileNameWithoutExtension(path);
+        }
+    }
+}
