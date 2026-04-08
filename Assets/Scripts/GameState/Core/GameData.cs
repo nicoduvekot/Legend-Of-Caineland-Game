9e2086c74f9fd5bbb578eb960ca9f5503e6a3129
Nico Duvekot <nduvekot7@gmail.com>
2026-04-08 15:05:10 -0700

    Create GameData
    Starting point for game data

---
+++ Assets/Scripts/GameState/Core/GameData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace GameState.Core
+{
+    /// <summary>
+    /// Authoritative Runtime Domain Layer
+    ///
+    /// Responsibilities:
+    ///     -Store the current game session's data
+    /// </summary>
+    public class GameData
+    {
+        public string SaveName { get; }
+        
+        public int PlayerHealth { get; internal set; }
+        public int MaxHealth { get; internal set; }
+        
+        public LevelId CurrentLevel { get; internal set; }
+        public PlayerId CurrentPlayer { get; internal set; }
+
+        public int CurrentCheckpoint { get; internal set; }
+        public int Coins { get; internal set; }
+
+        public HashSet<LevelId> LevelsUnlocked { get; internal set; } = new();
+        public HashSet<LevelId> LevelsCompleted { get; internal set; } = new();
+
+        // constructor
+        
+        public GameData(string saveName)
+        {
+            SaveName = saveName;
+        }
+    }
+}
