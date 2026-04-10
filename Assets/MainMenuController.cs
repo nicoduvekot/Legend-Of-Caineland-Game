f3c95aae322324167456de5c173c7579177d0d5c
Yoko Parks <hellothereyoko@gmail.com>
2026-04-09 18:23:31 -0700

    Main Menu Update
    Added:
    
    -Placeholder for Options, Load Screen
    -Modified Controller Code for Clarity
    -Patched that issue that kept merges from working

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -3,35 +3,47 @@ using UnityEngine.SceneManagement;
 
 /*
  * This script serves to control the main menu system in the game. 
- * I heavily modified a script given to me by Claude. It relied on older systems that weren't maintained so I adapted it to the
+ * I heavily modified a template script given to me by Claude. It relied on older systems that weren't maintained so I adapted it to the
  * new UI control methods and added additional UI debugs to help me while hooking it up!
  * 
- * Made by: Yoko Parks 
+ * The debug log statements tell the console to echo back when the buttons are pressed. This will tell if the code received input
+ *
+ *
+ * Made by: Yoko Parks
+ * Last Update: 04/09/2026 
+ * 
 */
 public class MainMenuController : MonoBehaviour
 {
-    [SerializeField] GameObject optionsPanel;
+    //[SerializeField] GameObject optionsPanel;
 
     //Mks new game & save. Then, it proceeds to load lvl 1
     public void NewGame()
     {
         Debug.Log("New Game Button Activated!");
+
         SceneManager.LoadScene("Level_01");
     }
+
 
     //Triggers Scene of the aforementioned name to load 
     public void LoadGame()
     {
       Debug.Log("Load Game Button Activated!");
-      SceneManager.LoadScene("Level_Load");
+      
+        SceneManager.LoadScene("Load_Save");
     }
+
 
     //Loads Options Menu
     public void OpenOptions()
     {
       Debug.Log("Options Button Activated!");
-      optionsPanel.SetActive(true);
+      //optionsPanel.SetActive(true);
+
+       SceneManager.LoadScene("Options");
     }
+
 
     //This one is pretty self-explainatory 
     public void QuitGame()
@@ -39,6 +51,8 @@ public class MainMenuController : MonoBehaviour
     {
       Debug.Log("Quit Button Activated!");
       Application.Quit();
+
+      //THIS BLOCK ALLOWS THE GAME CODE TO RUN IN TEST MODE DO NOT REMOVE
       #if UNITY_EDITOR
       UnityEditor.EditorApplication.isPlaying = false;
       #endif
