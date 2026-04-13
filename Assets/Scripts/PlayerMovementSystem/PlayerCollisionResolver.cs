e87145ec1e98d7792cf12f6bc0b6c0553041d3ec
Nico Duvekot <nduvekot7@gmail.com>
2026-04-13 00:51:18 -0700

    Update PlayerCollisionResolver
    It's in a workable starting state...but holy moly this is painful, and probably doing unprofessional coding at some parts lmao

--- Assets/Scripts/PlayerMovementSystem/PlayerCollisionResolver.cs
+++ Assets/Scripts/PlayerMovementSystem/PlayerCollisionResolver.cs
@@ -6,18 +6,30 @@ namespace PlayerMovementSystem
     /// <summary>
     /// This is the Collision Layer
     ///
-    /// Responsible for reporting collision resolutions
+    /// Responsible for taking the motor's weighted movement, and resolving if that movement can happen
+    ///
+    /// 1. Cast Rays in intended direction for length of that Velocity
+    /// 2. If Hit, resolve what must occur
+    /// 3. Apply adjustments to the velocity value
+    /// 4. Return the velocity value via CollisionInfo report
     /// </summary>
     [RequireComponent(typeof(BoxCollider2D))]
     public class PlayerCollisionResolver : MonoBehaviour
     {
         [Header("Collision Settings")]
         public LayerMask collisionMask;
-        public float skinWidth = 0.02f;
+        //public float skinWidth = 0.02f; // I might bring this back?
+        
+        [Header("Horizontal Ray Settings")]
+        public float horizontalRayVerticalOffset = 0.05f;
+
+        private CollisionInfo _info;
         
         private BoxCollider2D _collider;
         private RaycastOrigins _raycastOrigins;
         private readonly List<DebugRay> _debugRays = new();
+
+        //private const float Epsilon = 0.001f; // again. might bring this back?
         
         private void Awake()
         {
@@ -25,63 +37,174 @@ namespace PlayerMovementSystem
         }
         
         // Move is the core flow director for collision resolving
-        public CollisionInfo Move(Vector2 velocity)
+        public CollisionInfo Move(Vector2 displacement)
         {
             _debugRays.Clear();
-            UpdateRaycastOrigins();
+            _info.Reset();
+            UpdateColliderCorners();
             
-            CollisionInfo info = new();
-            info.Reset();
+            ResolveVerticalCollisions(ref displacement);
+            ResolveHorizontalCollisions(ref displacement);
             
-// TODO : REMOVE!! SIMPLE GROUND CHECK HERE FOR FUNCTION TEST 
-            Vector2 origin = (_raycastOrigins.bottomLeft + _raycastOrigins.bottomRight) * 0.5f;
-            Vector2 direction = Vector2.down;
-            float length = skinWidth + 0.05f;
-
-            RaycastHit2D hit = Physics2D.Raycast(origin, direction, length, collisionMask);
-
-            // store debug ray
-            _debugRays.Add(new DebugRay(origin, direction, length, hit));
-
-            if (hit)
-                info.Below = true;
-            
-// TODO : initial test : DO NOTHING
-            info.Velocity = velocity;
-
-            return info;
+            _info.Displacement = displacement;
+            return _info;
         }
         
-        private void UpdateRaycastOrigins()
+        private void UpdateColliderCorners()
         {
-            Bounds bounds = _collider.bounds;
-            bounds.Expand(skinWidth * -2);
+            Vector2 size = _collider.size;
+            Vector2 offset = _collider.offset;
+            
+            Vector2 scaledSize = new(
+                size.x * transform.lossyScale.x,
+                size.y * transform.lossyScale.y
+            );
+            
+            Vector2 half = scaledSize * 0.5f;
 
-            _raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-            _raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-            _raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-            _raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+            Vector2 localBL = offset + new Vector2(-half.x, -half.y);
+            Vector2 localBR = offset + new Vector2(+half.x, -half.y);
+            Vector2 localTL = offset + new Vector2(-half.x, +half.y);
+            Vector2 localTR = offset + new Vector2(+half.x, +half.y);
+            
+            _raycastOrigins.BottomLeft  = transform.TransformPoint(localBL);
+            _raycastOrigins.BottomRight = transform.TransformPoint(localBR);
+            _raycastOrigins.TopLeft     = transform.TransformPoint(localTL);
+            _raycastOrigins.TopRight    = transform.TransformPoint(localTR);
+        }
+
+        private void ResolveVerticalCollisions(ref Vector2 move)
+        {
+            if (move.y > 0)
+            {
+                _info.Below = false;
+                return;
+            }
+            
+            float rayLength = Mathf.Abs(move.y);
+            
+            RaycastHit2D[] downHits = CastDownRays(rayLength);
+
+            if (!TryFindClosestHit(downHits, out RaycastHit2D closestDownHit))
+            {
+                _info.Below = false;
+                return;
+            }
+            
+            float closestDistance = closestDownHit.distance;
+            
+            float clamped = Mathf.Min(Mathf.Abs(move.y), closestDistance);
+            move.y = -clamped;
+
+            _info.Below = true;
         }
         
+        private RaycastHit2D[] CastDownRays(float rayLength)
+        {
+            RaycastHit2D[] hits = new RaycastHit2D[5];
+            
+            Vector2 bl = _raycastOrigins.BottomLeft;
+            Vector2 br = _raycastOrigins.BottomRight;
+            
+            for (int i = 0; i < 5; i++)
+            {
+                float t = i / 4f;
+                Vector2 origin = Vector2.Lerp(bl, br, t);
+                Vector2 direction = Vector2.down;
+
+                RaycastHit2D hit = Physics2D.Raycast(origin, direction, rayLength, collisionMask);
+                hits[i] = hit;
+                
+                _debugRays.Add(new DebugRay(origin, direction, rayLength, hit));
+            }
+            return hits;
+        }
+
+        private void ResolveHorizontalCollisions(ref Vector2 move)
+        {
+            if (Mathf.Abs(move.x) < Mathf.Epsilon)
+                return;
+
+            float rayLength = Mathf.Abs(move.x);
+            float directionSign = Mathf.Sign(move.x);
+            
+            RaycastHit2D[] hits = CastHorizontalRays(rayLength, directionSign);
+            
+            if (!TryFindClosestHit(hits, out RaycastHit2D closestHit))
+                return;
+            
+            float closestDistance = closestHit.distance;
+            
+            float clamped = Mathf.Min(Mathf.Abs(move.x), closestDistance);
+            move.x = clamped * directionSign;
+            
+            if (directionSign < 0f)
+                _info.Left = true;
+            else
+                _info.Right = true;
+        }
+        
+        private RaycastHit2D[] CastHorizontalRays(float rayLength, float directionSign)
+        {
+            RaycastHit2D[] hits = new RaycastHit2D[5];
+            
+            Vector2 direction = directionSign < 0 ? Vector2.left : Vector2.right;
+            Vector2 bottom = directionSign < 0 ? _raycastOrigins.BottomLeft : _raycastOrigins.BottomRight;
+            Vector2 top = directionSign < 0 ? _raycastOrigins.TopLeft : _raycastOrigins.TopRight;
+            
+            bottom += Vector2.up * horizontalRayVerticalOffset;
+            
+            for (int i = 0; i < 5; i++)
+            {
+                float t = i / 4f;
+                Vector2 origin = Vector2.Lerp(bottom, top, t);
+
+                RaycastHit2D hit = Physics2D.Raycast(origin, direction, rayLength, collisionMask);
+                hits[i] = hit;
+
+                _debugRays.Add(new DebugRay(origin, direction, rayLength, hit));
+            }
+            
+            return hits;
+        }
+
+        private static bool TryFindClosestHit(RaycastHit2D[] hits, out RaycastHit2D closestHit)
+        {
+            bool hasHit = false;
+            float closestDist = float.MaxValue;
+            closestHit = default;
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (!hit) continue;
+                if (!(hit.distance < closestDist)) continue;
+                
+                closestDist = hit.distance;
+                closestHit = hit;
+                hasHit = true;
+            }
+            return hasHit;
+        }
+
         private struct RaycastOrigins
         {
-            public Vector2 topLeft, topRight;
-            public Vector2 bottomLeft, bottomRight;
+            public Vector2 TopLeft, TopRight;
+            public Vector2 BottomLeft, BottomRight;
         }
         
         private struct DebugRay
         {
-            public Vector2 origin;
-            public Vector2 direction;
-            public float length;
-            public RaycastHit2D hit;
+            public readonly Vector2 Origin;
+            public readonly Vector2 Direction;
+            public readonly float Length;
+            public readonly RaycastHit2D Hit;
 
             public DebugRay(Vector2 origin, Vector2 direction, float length, RaycastHit2D hit)
             {
-                this.origin = origin;
-                this.direction = direction;
-                this.length = length;
-                this.hit = hit;
+                Origin = origin;
+                Direction = direction;
+                Length = length;
+                Hit = hit;
             }
         }
         
@@ -92,11 +215,11 @@ namespace PlayerMovementSystem
 
             foreach (DebugRay r in _debugRays)
             {
-                Gizmos.color = r.hit ? Color.red : Color.green;
-                Gizmos.DrawLine(r.origin, r.origin + r.direction.normalized * r.length);
+                Gizmos.color = r.Hit ? Color.red : Color.green;
+                Gizmos.DrawLine(r.Origin, r.Origin + r.Direction.normalized * r.Length);
 
-                if (r.hit)
-                    Gizmos.DrawSphere(r.hit.point, 0.03f);
+                if (r.Hit)
+                    Gizmos.DrawSphere(r.Hit.point, 0.03f);
             }
         }
     }
