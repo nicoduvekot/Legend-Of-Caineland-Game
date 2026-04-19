393abceeb688e9f5b61e02f4d012b80771a1b254
Nico Duvekot <nduvekot7@gmail.com>
2026-04-19 15:46:13 -0700

    Update GameFlowManagerEditor
    Updated Editor tools to reflect the updated script

--- Assets/Scripts/GameState/Editor/GameFlowManagerEditor.cs
+++ Assets/Scripts/GameState/Editor/GameFlowManagerEditor.cs
@@ -6,33 +6,36 @@ namespace GameState.Editor
     [CustomEditor(typeof(GameFlowManager))]
     public class GameFlowManagerEditor : UnityEditor.Editor
     {
-        private string _sceneName;
+        private string _saveName;
 
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
-            
             EditorGUILayout.Space();
             
-            _sceneName = EditorGUILayout.TextField("Scene Name", _sceneName);
-            
-            if (Application.isPlaying)
-            {
-                if (GUILayout.Button("Trigger OnLevelStarted"))
-                {
-                    GameFlowManager manager = (GameFlowManager)target;
-                    manager.SendMessage("OnLevelStarted", _sceneName);
-                }
-                
-                if (GUILayout.Button("Trigger Player Death"))
-                {
-                    GameFlowManager manager = (GameFlowManager)target;
-                    manager.SendMessage("OnPlayerDeath");
-                }
-            }
-            else
+            GameFlowManager manager = (GameFlowManager)target;
+
+            if (!Application.isPlaying)
             {
                 EditorGUILayout.HelpBox("Enter Play Mode to use debug tools.", MessageType.Info);
+                return;
+            }
+            
+            if (GUILayout.Button("Start New Game"))
+            {
+                manager.StartNewGame();
+            }
+            
+            _saveName = EditorGUILayout.TextField("Save Name", _saveName);
+
+            if (GUILayout.Button("Load Game"))
+            {
+                manager.LoadGame(_saveName);
+            }
+            
+            if (GUILayout.Button("Trigger Player Death"))
+            {
+                manager.OnPlayerDeath();
             }
         }
     }
